43c6313dd57112f59e0bd54ecd5456aa2b813d9d
Matheus Gois <matheus.gois@btgpactual.com>
2023-07-12 22:58:07 -0300

    main feat(Main): Dynamic Search

--- src/api/extraction/extraction.controller.ts
+++ src/api/extraction/extraction.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Body, Delete, Put, Req, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Body, Delete, Put, Req, Param, Query } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AnswerDTO } from 'src/dto/answer/answer.dto';
 import { ExtractionDTO } from 'src/dto/models/extraction.dto';
 import { ExtractionService } from './extraction.service';
@@ -16,9 +16,10 @@ export class ApiExtractionController {
      * Get the Extraction by you userId
      */
     @Get()
-    async get(@Req() req): Promise<AnswerDTO<ExtractionDTO[]>> {
+    @ApiQuery({ name: 'search', description: 'Selecione a busca', required: true })
+    async get(@Query('search') search): Promise<AnswerDTO<ExtractionDTO[]>> {
         try {
-            const data = await this.extractionService.getAll()
+            const data = await this.extractionService.getAll(search)
             return new AnswerDTO('Resumos lidos com sucesso!', 200, data);
         } catch (error) {
             throw error.message || error
--- src/api/extraction/extraction.service.ts
+++ src/api/extraction/extraction.service.ts
@@ -11,13 +11,7 @@ export class ExtractionService {
         private readonly crawler: NestCrawlerService,
     ) { }
 
-    async getAll(): Promise<any> {
-        interface ExampleCom {
-            title: string;
-            year: string;
-            authors: string;
-            abstract: string;
-        }
+    async getAll(word: string): Promise<any> {
 
         interface Page {
             title: string;
@@ -25,23 +19,32 @@ export class ExtractionService {
 
         const pages: Page[] = await this.crawler.fetch({
             target: {
-                url: 'https://sol.sbc.org.br/busca/index.php/integrada/results?isAdvanced=1&archiveIds%5B%5D=1&archiveIds%5B%5D=2&query=plataforma+de+an%C3%A1lise+de+redes+sociais&field-3=&field-15=&field-4=&field-14=&field-16=&field-7-fromMonth=01&field-7-fromDay=1&field-7-fromYear=2018&field-7-toMonth=&field-7-toDay=&field-7-toYear=',
+                url: `https://sol.sbc.org.br/busca/index.php/integrada/results?query=${encodeURIComponent(word)}`,
                 iterator: {
                     selector: 'a.record_title'
                 },
             },
             fetch: (data: any, index: number, url: string) => {
-                console.log(url)
                 return {
-                    title: 'h1',
-                    year: 'div.published',
-                    authors: 'span.name',
-                    abstract: 'div.abstract'
+                    title: {
+                        selector: 'h1',
+                        convert: (x: string) => `${x}`,
+                    },
+                    year: {
+                        selector: 'div.published',
+                        convert: (x: string) => `${x.replace(/Publicado/g, '').replace(/\n/g, '').replace(/\t/g, '').replace(/.*\/(\d{4})$/, "$1")}`,
+                    },
+                    authors: {
+                        selector: 'span.name',
+                        convert: (x: string) => `${x.replace(/\t/g, "").replace(/\n\n/g, ', ')}.`,
+                    },
+                    abstract: {
+                        selector: 'div.abstract',
+                        convert: (x: string) => `${x.replace(/\t/g, "").replace(/Resumo\n/g, '')}`,
+                    }
                 }
             },
         });
-
-        console.log(pages);
 
         return pages
     }
