8b71895116274111833f83dabc5ebac0d802ba7c
Matheus Gois <matheus.gois@btgpactual.com>
2023-07-22 14:49:49 -0300

    main feat(Main): New Updates ✨

--- public/js/main.js
+++ public/js/main.js
@@ -33,23 +33,3 @@
     console.log("Loaded...")
   });
 })(jQuery); // End of use strict
-
-function tableExport(type) {
-  const fileName = $('#inputWord').val()
-  $('#dataTable').tableExport(
-    {
-      fileName,
-      type,
-      onCellHtmlData: function (cell, rowIndex, colIndex, htmlData) {
-        if ($(cell).find('a').length > 0) {
-          let link = $(cell).find('a').attr('href')
-          if (colIndex == 0) {
-            return htmlData;
-          } else {
-            return link;
-          }
-        }
-        return htmlData;
-      }
-    });
-}
