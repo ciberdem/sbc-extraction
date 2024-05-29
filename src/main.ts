8be0dd0f62d7c609d45f74f14567056bcbd4b23c
Matheus Gois <matheus.gois@btgpactual.com>
2024-05-29 20:57:28 -0300

    feat(Server-Apps): New Updates ✨

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
-import { ApiExtractionModule } from './modules/extraction/extraction.module';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { json } from 'body-parser';
 import { join } from 'path';
 import * as hbs from 'hbs';
 import * as helmet from 'helmet';
-
 import * as session from 'express-session';
 import flash = require('connect-flash');
 import CustomHelpers from './helpers/hbs/hbsHelpers';
@@ -24,28 +21,23 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, options, {
-    include: [
-      ApiExtractionModule,
-    ]
-  });
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  //Limit 
+  // Limit 
   app.use(json({ limit: '50mb' }));
+  
   // Validations
   app.useGlobalPipes(new ValidationPipe());
-  // Cors
-  // app.enableCors();
-  // Add filters
-  // app.useGlobalFilters(new AllExceptionsFilter())
-  //VIEW - HBS
+
+  // Serve static assets from the 'public' directory
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  
+  // View engine setup
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views/partials'));
   hbs.registerHelper('formatDate', CustomHelpers.formatDate);
-
   hbs.registerHelper('contains', function (array, value, options) {
     if (array && array.indexOf(value) !== -1) {
       return options.fn(this);
@@ -54,19 +46,10 @@ async function bootstrap() {
     }
   });
 
-  // Passaport Session
-  // app.use(session({
-  //   secret: 'AKSJNDKJSANDJ@#$%^&UYHGFDE$@R%TYHu3y12g3i87gbjuasd87g1ouj2v9ey',
-  //   cookie: {
-  //     maxAge: 86400000 * 7 * 12, // 1 ano
-  //   }
-  // }))
+  // Helmet for security headers
+  app.use(helmet({ contentSecurityPolicy: false }));
 
-  // app.use(flash());
-  // app.use(helmet({ contentSecurityPolicy: false }));
-  // TODO: - Improve: helmet({ contentSecurityPolicy: { directives: { 'script-src': ["'self'", "https://whitelisted-domain.com"] } } })
-
-  // PORT
-  await app.listen(process.env.PORT || 3000);
+  // Start the application
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
